0497fe49e053fc32465930f09aecd665c1350678
iceChen8123 <81231707@qq.com>
2016-06-17 18:06:26 +0800

    蛇蛇

---
+++ src/main/java/ice/snake/SnakeServer.java
@@ -0,0 +1,34 @@
+package ice.snake;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.atmosphere.nettosphere.Config;
+import org.atmosphere.nettosphere.Nettosphere;
+import org.nettosphere.samples.games.SnakeManagedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SnakeServer {
+
+	private static final Logger logger = LoggerFactory.getLogger(Nettosphere.class);
+
+	public static void main(String[] args) throws IOException {
+		Config.Builder b = new Config.Builder();
+		b.resource(SnakeManagedService.class).resource("D:\\workspacem\\snake\\src\\main\\resources").port(8080)
+				.host("127.0.0.1").build();
+		Nettosphere s = new Nettosphere.Builder().config(b.build()).build();
+		s.start();
+		String a = "";
+
+		logger.info("NettoSphere Games Server started on port {}", 8080);
+		logger.info("Type quit to stop the server");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (!(a.equals("quit"))) {
+			a = br.readLine();
+		}
+		System.exit(-1);
+	}
+
+}
---
+++ src/main/java/org/nettosphere/samples/games/Settings.java
@@ -0,0 +1,6 @@
+package org.nettosphere.samples.games;
+
+public class Settings {
+	public static final int PLAYFIELD_WIDTH = 800;
+	public static final int PLAYFIELD_HEIGHT = 600;
+}
