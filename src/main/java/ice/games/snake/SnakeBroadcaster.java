1531075f8ffea68577b04b2305db80a0c1f8a48d
iceChen8123 <81231707@qq.com>
2016-06-20 17:53:34 +0800

    删掉个无用的

--- src/main/java/ice/games/snake/SnakeBroadcaster.java
+++ src/main/java/ice/games/snake/SnakeBroadcaster.java
@@ -31,87 +31,78 @@
  */
 package ice.games.snake;
 
-import org.atmosphere.cpr.Broadcaster;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.Timer;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+
+import org.atmosphere.cpr.Broadcaster;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Sets up the timer for the multi-player snake game WebSocket example.
  */
 public class SnakeBroadcaster {
 
-    private final static Logger logger = LoggerFactory.getLogger(SnakeBroadcaster.class);
+	private final static Logger logger = LoggerFactory.getLogger(SnakeBroadcaster.class);
 
-    private Timer gameTimer = null;
+	private final long TICK_DELAY = 100;
 
-    private final long TICK_DELAY = 100;
+	private final ConcurrentHashMap<Integer, Snake> snakes = new ConcurrentHashMap<Integer, Snake>();
 
-    private final ConcurrentHashMap<Integer, Snake> snakes =
-            new ConcurrentHashMap<Integer, Snake>();
+	private final Broadcaster broadcaster;
 
-    private final Broadcaster broadcaster;
+	public SnakeBroadcaster(Broadcaster broadcaster) {
+		this.broadcaster = broadcaster;
+	}
 
-    public SnakeBroadcaster(Broadcaster broadcaster) {
-        this.broadcaster = broadcaster;
-    }
+	protected synchronized void addSnake(Snake snake) {
+		if (snakes.size() == 0) {
+			startTimer();
+		}
+		snakes.put(Integer.valueOf(snake.getId()), snake);
+	}
 
-    protected synchronized void addSnake(Snake snake) {
-        if (snakes.size() == 0) {
-            startTimer();
-        }
-        snakes.put(Integer.valueOf(snake.getId()), snake);
-    }
+	protected Collection<Snake> getSnakes() {
+		return Collections.unmodifiableCollection(snakes.values());
+	}
 
+	protected synchronized void removeSnake(Snake snake) {
+		snakes.remove(Integer.valueOf(snake.getId()));
+	}
 
-    protected Collection<Snake> getSnakes() {
-        return Collections.unmodifiableCollection(snakes.values());
-    }
+	protected String tick() {
+		StringBuilder sb = new StringBuilder();
+		for (Iterator<Snake> iterator = getSnakes().iterator(); iterator.hasNext();) {
+			Snake snake = iterator.next();
+			snake.update(getSnakes());
+			sb.append(snake.getLocationsJson());
+			if (iterator.hasNext()) {
+				sb.append(',');
+			}
+		}
+		return String.format("{'type': 'update', 'data' : [%s]}", sb.toString());
+	}
 
+	public SnakeBroadcaster broadcast(String message) {
+		broadcaster.broadcast(message);
+		return this;
+	}
 
-    protected synchronized void removeSnake(Snake snake) {
-        snakes.remove(Integer.valueOf(snake.getId()));
-    }
-
-
-    protected String tick() {
-        StringBuilder sb = new StringBuilder();
-        for (Iterator<Snake> iterator = getSnakes().iterator();
-             iterator.hasNext(); ) {
-            Snake snake = iterator.next();
-            snake.update(getSnakes());
-            sb.append(snake.getLocationsJson());
-            if (iterator.hasNext()) {
-                sb.append(',');
-            }
-        }
-        return String.format("{'type': 'update', 'data' : [%s]}",
-                sb.toString());
-    }
-
-    public SnakeBroadcaster broadcast(String message) {
-        broadcaster.broadcast(message);
-        return this;
-    }
-
-    public void startTimer() {
-        broadcaster.scheduleFixedBroadcast(new Callable<String>() {
-            @Override
-            public String call() {
-                try {
-                    return tick();
-                } catch (RuntimeException e) {
-                    logger.error("Caught to prevent timer from shutting down", e);
-                }
-                return "";
-            }
-        }, TICK_DELAY, TICK_DELAY, TimeUnit.MILLISECONDS);
-    }
+	public void startTimer() {
+		broadcaster.scheduleFixedBroadcast(new Callable<String>() {
+			@Override
+			public String call() {
+				try {
+					return tick();
+				} catch (RuntimeException e) {
+					logger.error("Caught to prevent timer from shutting down", e);
+				}
+				return "";
+			}
+		}, TICK_DELAY, TICK_DELAY, TimeUnit.MILLISECONDS);
+	}
 }
